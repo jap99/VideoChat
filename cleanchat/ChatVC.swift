e49a084a46a341fce174ce5e5cdbebe824ddb6fc
javidp.sf@gmail.com <javidpoornasir@Javids-MacBook-Pro-2.local>
2018-04-13 01:20:32 -0700

    chat view controller pt 2

--- cleanchat/ChatVC.swift
+++ cleanchat/ChatVC.swift
@@ -20,6 +20,27 @@ class ChatVC: JSQMessagesViewController, UINavigationControllerDelegate, UIImage
     var min = 0
     
     var messages: [JSQMessage] = []
+    var objects: [NSDictionary] = []
+    var loaded: [NSDictionary] = [] // to load a small amount of messages to avoid user having to wait longer than necessary
+    
+    var avatarImagesDictionary: NSMutableDictionary? // for saving jsq avatar objects to represent avatar images
+    var avatarDictionary: NSMutableDictionary? // dictionary of our image data objects
+    
+    var members: [String] = [] // chat members - only holds they IDs
+    var withUsers: [BackendlessUser] = []
+    var titleName: String?
+    
+    var chatRoomId: String!
+    var isGroup: Bool?
+    
+    var initialLoadComplete: Bool = false
+    var showAvatars = true
+    var firstLoad: Bool?
+    
+    
+    // put two jsq message bubbles
+    let outgoingBubble = JSQMessagesBubbleImageFactory().outgoingMessagesBubbleImage(with: UIColor.jsq_messageBubbleBlue())
+    let incomingBubble = JSQMessagesBubbleImageFactory().incomingMessagesBubbleImage(with: UIColor.jsq_messageBubbleLightGray())
     
     override func viewDidLoad() {
         super.viewDidLoad()
