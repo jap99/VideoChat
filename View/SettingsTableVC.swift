4e4686730e0f20d94666d839e339016214446348
javidp.sf@gmail.com <javidpoornasir@Javids-MacBook-Pro-2.local>
2018-04-24 02:22:13 -0700

    show avatar custom cell

--- View/SettingsTableVC.swift
+++ View/SettingsTableVC.swift
@@ -85,10 +85,9 @@ class SettingsTableVC: UITableViewController {
             return cell
         }
         
-        return UITableViewCell
-    }
+        return UITableViewCell()
     
-
+    }
     /*
     // Override to support conditional editing of the table view.
     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
---
+++ View/ShowAvatarCell.swift
@@ -0,0 +1,71 @@
+//
+//  ShowAvatarCell.swift
+//  cleanchat
+//
+//  Created by Javid Poornasir on 4/24/18.
+//  Copyright © 2018 Javid Poornasir. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ShowAvatarCell: UITableViewCell {
+
+    @IBOutlet weak var showAvatarSwitch: UISwitch!
+    
+    let userDefaults = userDefaults.standard
+    var avatarSwitchStatus = true
+    var firstLoad: Bool?
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        loadUserDefaults()
+    }
+
+    
+    @IBAction func avatarSwitchStatusChanged(_ sender: UISwitch) {
+        
+        if sender.isOn {
+            avatarSwitchStatus = true
+        } else {
+            avatarSwitchStatus = false
+        }
+        
+        saveUserDefaults()
+    }
+    
+    // MARK: UserDefaults
+    
+    func saveUserDefaults() {
+        // gets our avatar image state and saves to userdefaults
+        userDefaults.set(avatarSwitchStatus, forKey: kAVATARSTATE)
+        userDefaults.synchronize()
+    }
+    
+    func loadUserDefaults() { // gets our status from user defaults and puts switch in correct position
+        
+        firstLoad = userDefaults.bool(forKey: kFIRSTRUN)
+        
+        if !firstLoad! {
+            userDefaults.set(true, forKey: kFIRSTRUN)
+            userDefaults.set(avatarSwitchStatus, forKey: kAVATARSTATE)
+            userDefaults.synchronize()
+        }
+        
+        avatarSwitchStatus = userDefaults.bool(forKey: kAVATARSTATE)
+        showAvatarSwitch.isOn = avatarSwitchStatus
+    }
+    
+
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    
+    
+    
+    
+}
