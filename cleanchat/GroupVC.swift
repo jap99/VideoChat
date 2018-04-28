15f8e236638dc23f8bbe297676dd0b2a9d98bb17
javidp.sf@gmail.com <javidpoornasir@Javids-MacBook-Pro-2.local>
2018-04-27 20:58:11 -0700

    get back the group from firebase upon loading

--- cleanchat/GroupVC.swift
+++ cleanchat/GroupVC.swift
@@ -18,6 +18,7 @@ class GroupVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        loadGroups()
        
     }
 
@@ -35,8 +36,9 @@ class GroupVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         
         // get the group for the specific cell
         let group = groups[indexPath.row]
-        
+        let membersCount = (group[kMEMBERS] as? [String])!.count
         cell.textLabel?.text = group[kNAME] as? String // group name
+        cell.detailTextLabel?.text = "\(membersCount) members"
         
         return cell
     }
@@ -60,6 +62,33 @@ class GroupVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         // called when user clicks the delete button
     }
     
+    
+    // MARK: Load groups
+    
+    func loadGroups() {
+        
+        // get all groups from firebase
+        // query all groups that belong to current user
+        firebase.child(kGROUP).queryOrdered(byChild: kOWNERID).queryEqual(toValue: backendless!.userService.currentUser.objectId!).observe(.value) { (snapshot) in
+            
+            self.groups.removeAll()
+            
+            if snapshot.exists() {
+                
+                // create an array and sort by the date they were created
+                let sorted = ((snapshot.value! as! NSDictionary).allValues as NSArray).sortedArray(using: [NSSortDescriptor(key: kDATE, ascending: false)])
+                
+                // go through groups
+                for group in sorted {
+                    
+                    self.groups.append(group as! NSDictionary)
+                    
+                }
+            }
+            
+            self.tv.reloadData() // reloaded when add a new group to our array
+        }
+    }
     
     
     // MARK: IBActions
