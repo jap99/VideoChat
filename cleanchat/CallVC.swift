13f0fd0e6d5e5150ab4865c63ae0cda1e2411a19
javidp.sf@gmail.com <javidpoornasir@Javids-MacBook-Pro-2.local>
2018-05-02 18:02:55 -0700

    call vc part 2

--- cleanchat/CallVC.swift
+++ cleanchat/CallVC.swift
@@ -13,15 +13,88 @@ class CallVC: UIViewController, SINCallDelegate {
     @IBOutlet weak var remoteUserNameLabel: UILabel!
     @IBOutlet weak var statusLabel: UILabel!
     
+    @IBOutlet weak var declineButton: UIButton!
+    @IBOutlet weak var hangupButton: UIButton!
+    @IBOutlet weak var answerButton: UIButton!
+    
+    var durationTimer: Timer! = nil
+    var _call: SINCall
+    
+    var callAnswered = false // used to know which UI to display
+    
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate // since we're setting up call in app delegate later on
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         
     }
-
     
-    // IBActions
+    // MARK: UI Update
+
+    func setCallStatusText(text: String) {
+        
+        statusLabel.text = text // passing our status to our UI
+    }
+    
+    func showButtons() {
+        
+        // answered call or is calling
+        if callAnswered {
+            declineButton.isHidden = true
+            hangupButton.isHidden = false
+            answerButton.isHidden = true
+            
+        } else {
+            
+            declineButton.isHidden = false
+            hangupButton.isHidden = true
+            answerButton.isHidden = false
+        }
+    }
+    
+    // MARK: Helper
+    
+    // returns the path of the sounds to be played
+    func pathForSound(soundName: String) -> String {
+        
+        return Bundle.main.path(forResource: soundName, ofType: ".wav")!
+    }
+    
+    // MARK: Timer
+    
+    @objc func onDurationTimer() {
+        
+        let duration = Date().timeIntervalSince(_call.details.establishedTime) // shows the call duration
+        updateTimerLabel(seconds: Int(duration))
+    }
+    
+    func updateTimerLabel(seconds: Int) {
+        
+        let min = String(format: "%02d", (seconds / 60))  // get rid of the zeros after the decimal zeros
+        let sec = String(format: "%02d", (seconds % 60))  // leaves two decimal values at the end
+        
+        setCallStatusText(text: "\(min) : \(sec)")
+    }
+    
+    func startCallDurationTimer() {
+        // first every half seconds, it's on our VC, we'll have a selector
+        self.durationTimer = Timer.scheduledTimer(timeInterval: 0.5, target: self, selector: #selector(self.onDurationTimer), userInfo: nil, repeats: true)
+    }
+    
+    func stopCallDurationTimer() {
+        
+        // check if caller stops call before we could answer it
+        if durationTimer != nil { // timer was instantiated
+            
+            durationTimer.invalidate()
+            durationTimer = nil
+        }
+    }
+    
+    // MARK: IBActions
     
     @IBAction func declineButton_Pressed(_ sender: UIButton) {
         
